f4877b17953bb445d2b99cfa90b8022269aef0cf
DenzelWashington <denzelwashingtonsantana@gmail.com>
2022-08-07 01:13:50 -0300

    inserindo informação

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,16 @@
-import React from 'react'
+
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="flex flex-col items-center text-center h-screen">
+      <h2 className='text-2xl font-bold mb-5 '>
+        Sobre o Logotrip
+      </h2>
+      <p className='text-gray-400 mb-6 text-lg'> 
+        Este projeto consiste em um site de agência de viagens com o foco na interação com clientes.
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
