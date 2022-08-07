d0e1c72729ecb073539699be53639f4b47828e29
DenzelWashington <denzelwashingtonsantana@gmail.com>
2022-08-07 01:17:44 -0300

    Atualizando registro

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,9 +1,102 @@
-import React from 'react'
+
+import { useEffect, useState } from "react";
+import { useAuthentication } from "../../hooks/useAuthentication";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
 
-export default Register
+  const { createUser, error: authError, loading } = useAuthentication();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError("");
+
+    const user = {
+      displayName,
+      email,
+      password,
+    };
+
+    if (password !== confirmPassword) {
+      setError("As senhas precisam ser iguais.");
+      return;
+    }
+
+    const res = await createUser(user);
+
+    console.log(res);
+  };
+
+  useEffect(() => {
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
+
+  return (
+    <div className="flex flex-col items-center text-center h-screen">
+      <h1 className='text-2xl font-bold mb-5 '>Cadastre-se para postar</h1>
+      <p className='text-gray-400 mb-6'>Crie seu usuário e compartilhe suas histórias</p>
+      <form className="flex flex-col items-center max-w-[50%]" onSubmit={handleSubmit}>
+        <label>
+          <span>Nome:</span>
+          <input
+            type="text"
+            name="displayName"
+            required
+            placeholder="Nome do usuário"
+            onChange={(e) => setDisplayName(e.target.value)}
+            value={displayName}
+          />
+        </label>
+        <label>
+          <span>E-mail:</span>
+          <input
+            type="email"
+            name="email"
+            required
+            placeholder="E-mail do usuário"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input
+            type="password"
+            name="password"
+            required
+            placeholder="Insira a senha"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </label>
+        <label>
+          <span>Confirmação de senha:</span>
+          <input
+            type="password"
+            name="confirmPassword"
+            required
+            placeholder="Confirme a senha"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            value={confirmPassword}
+          />
+        </label>
+        {!loading && <button  className=' bg-emerald-300 py-2 mb-5 w-20 text-sm text-gray-800 rounded-xl'>Entrar</button>}
+        {loading && (
+          <button className=' bg-gray-50 py-2 mb-5 w-20 text-sm text-gray-800 rounded-xl' disabled>
+            Aguarde...
+          </button>
+        )}
+        {error && <p className='text-red-900 bg-red-200 p-1 rounded text-center'>{error}</p>}
+      </form>
+    </div>
+  );
+};
+
+export default Register;
