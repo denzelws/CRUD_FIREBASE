4b234177f486d9f12d66092cc4d964fb8e0d2d25
DenzelWashington <denzelwashingtonsantana@gmail.com>
2022-08-06 21:09:51 -0300

    Atualizando regras para o navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,47 +1,78 @@
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import { useAuthentication } from '../hooks/useAuthentication'
+import { useAuthValue } from '../context/AuthContext'
 
-function NavBar() {
+const NavBar = () => {
+
+  const { logout } = useAuthentication();
+  const { user } = useAuthValue();
+
   return (
-      <nav className='container mx-auto px-4 h-24 flex items-center'>
-        <span className='font-black cursor-pointer'>
-          Logotrip
-        </span>
+    <nav className='container mx-auto px-4 h-24 flex items-center'>
+      <span className='font-black cursor-pointer'>
+        Logotrip
+      </span>
 
-        <ul className='ml-auto flex gap-4'>
-          
-            <li className='text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer'>
-              <NavLink to='/'>
-              Home
-              </NavLink>
-            </li>
 
+      <ul className='ml-auto flex gap-4'>
+        <li className='text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer'>
+          <NavLink to='/'>
+            Home
+          </NavLink>
+        </li>
+        {!user && (
+          <>
             <li className='text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer'>
               <NavLink to='/login'>
-              Entrar
+                Entrar
               </NavLink>
             </li>
 
             <li className='text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer'>
               <NavLink to='/register'>
-              Cadastrar
+                Cadastrar
               </NavLink>
             </li>
+            </>
+        )}
+          {user && (
+              <>
+                <li className='text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer'>
+                  <NavLink to='/posts/create'>
+                    Novo Post
+                  </NavLink>
+                </li>     
+              </>
+            )}
+             <li className='text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer'>
+                  <NavLink to='/posts/feed'>
+                    Feedback/Postagens
+                  </NavLink>
+                </li>           
+        {user && (
+          <>
+          <li className='text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer'> 
+           <NavLink
+             to="/dashboard"
+           >
+             Dashboard
+           </NavLink>
+         </li>
+         </>
+        )}
+        <li className='text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer'>
+          <NavLink to='/about'>
+            Sobre
+          </NavLink>
+        </li>
+        {user && (
+           <button 
+           className='text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer' 
+           onClick={logout}>Sair</button>
+        )}     
+      </ul>
+    </nav>
 
-            <li className='text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer'>
-              <NavLink to='/feed'>
-              Postagens/Feedback
-              </NavLink>
-            </li>
-
-            <li className='text-sm text-gray-600 hover:text-gray-800 hover:underline cursor-pointer'>
-              <NavLink to='/about'>
-              Sobre
-              </NavLink>
-            </li>
-
-        </ul>
-      </nav>
-    
 
   )
 }
