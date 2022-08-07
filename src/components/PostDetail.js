01e9bec48b211df75121661482fc9790ed5abf64
DenzelWashington <denzelwashingtonsantana@gmail.com>
2022-08-07 01:15:43 -0300

    adicionando estilos

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -3,19 +3,21 @@ import { Link } from "react-router-dom";
 
 const PostDetail = ({ post }) => {
   return (
-    <div className={styles.post_detail}>
-      <img src={post.image} alt={post.title} />
-      <h2>{post.title}</h2>
-      <p className={styles.createdby}>por: {post.createdBy}</p>
-      <div className={styles.tags}>
-        {post.tags.map((tag) => (
+    <div className="flex flex-col items-center text-center">
+      <img className="max-w-xl" src={post.image} alt={post.title} />
+      <h2 className="text-xl">{post.title}</h2>
+      <p className="italic text-gray-400 text-sm mb-2"> por: {post.createdBy}</p>
+      <div className="flex mb-2">
+        {post.tagsArray.map((tag) => (
           <p key={tag}>
-            <span>#</span>
+            <span className="font-semibold">#</span>
             {tag}
           </p>
         ))}
       </div>
-      <Link to={`/posts/${post.id}`} className="btn btn-outline">
+      <Link 
+      className="bg-white border border-current hover:bg-gray-100 py-1 px-5 rounded-md mb-4"
+      to={`/posts/${post.id}`}>
         Ler
       </Link>
     </div>
